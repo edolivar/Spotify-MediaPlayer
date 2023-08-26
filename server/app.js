e5ad588587b760353cb66868c4d393b0f50ce095
Edwin Olivares <edwin@Edwins-MacBook-Pro.local>
2023-08-26 12:03:17 -0700

    added the correct uri

--- server/app.js
+++ server/app.js
@@ -1,14 +1,10 @@
 import express from "express"
 import cors from 'cors'
-import fetch from "node-fetch"
-import querystring from 'query-string'
-import { Buffer } from 'buffer'
 import path from "path"
 import { dirname } from "path"
 import { fileURLToPath } from 'url';
 
 const clientID = 'c0a1baade757484da9b2fd790d541f4f'
-const clientSecret = '7c4ad0165ce546e098b42e450be697ab'
 //const TOKEN_URL = `https://accounts.spotify.com/api/token`
 //const basicAuth = Buffer.from(`${clientID}:${clientSecret}`).toString('base64')
 const __filename = fileURLToPath(import.meta.url);
@@ -42,8 +38,4 @@ app.post('/api/accessTokenTest', (req, res) => {
 
 })
 
-app.get('/api/spotifyInfo/', (req, res) => {
-    const rTok = req.query.rTok
-    songHandler(rTok).then(resp => res.send(resp))
-})
 
